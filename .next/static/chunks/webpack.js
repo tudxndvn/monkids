5b1b294a230935066a6832d18ef745631874b841
Tu DX <tudxndvn@gmail.com>
2023-05-06 14:05:44 +0700

    create page faq

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "161ac91283099165"; }
+/******/ 		__webpack_require__.h = function() { return "10185fd48f97e16b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
