826c927e89809985d515f7fca49e93aef69a13db
Tu DX <tudxndvn@gmail.com>
2023-05-06 15:43:08 +0700

    create home page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c977dff5212ca8de"; }
+/******/ 		__webpack_require__.h = function() { return "fc1cf0990460abb4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
