64542676597e7b39a2b09b644ea9cdffc52be3fa
Tu DX <tudxndvn@gmail.com>
2023-05-06 13:18:10 +0700

    create page privacy-policy

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ba018fc5596247ca"; }
+/******/ 		__webpack_require__.h = function() { return "470b048e0fb7d39d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
